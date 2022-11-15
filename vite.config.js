d75c28255eefe59facc1dc1e286e05ec166455c8
billy4479 <giachi.ellero@gmail.com>
2022-11-15 11:27:32 +0100

    Update deps and add wasm support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+
 import { createHtmlPlugin } from 'vite-plugin-html';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
   plugins: [
+    wasm(),
+    topLevelAwait(),
     svelte({
       preprocess: [sveltePreprocess({ typescript: true })],
     }),
